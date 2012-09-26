e67dd34fe80ff2eb24cd9ee5f83260cffe737c54
Bill DeRusha <bill.derusha@gmail.com>
2012-09-26 19:06:45 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImprovAuditions::Application.config.secret_token = '449648d29a1fd8025071fa2d88912a7e3e9889029969505080b400990d21c9f02585cd06da466ebc8f5fde73b99a4a1f3b7e0b1dca084da535633cd0961d6603'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ImprovAuditions::Application.config.session_store :cookie_store, key: '_ImprovAuditions_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ImprovAuditions::Application.config.session_store :active_record_store
